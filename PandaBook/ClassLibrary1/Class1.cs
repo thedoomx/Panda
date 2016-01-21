f4fba337a69872265ef72761444f5ec98ee45137
Bzahov98 <bzahov98.abv.bg>
2016-01-21 18:18:30 +0200

    add panda

---
+++ PandaBook/ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panda
+{
+	public class Panda
+	{
+		private string Name;
+		private string Email;
+
+		private GenderType Gender;
+
+		public enum GenderType
+		{
+			Male,
+			Female,
+		};
+
+		Panda()
+		{
+			Name = "";
+			Email = "";
+			Gender = GenderType.Male;
+		}
+
+		Panda(string name,string email, GenderType gender)
+		{
+			Name = name;
+			Email = email;
+			Gender = gender;
+		}
+	}
+}
