b00856827d0d8ad8acacde9706f7bf78256da72a
thedoomx <stani_b_sf@abv.bg>
2016-01-21 21:15:26 +0200

    namespace fix

--- PandaBook/SocialNetwork/PandaSocialNetwork.cs
+++ PandaBook/SocialNetwork/PandaSocialNetwork.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using PandaLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SocialNetwork
+namespace SocialNetworkLibrary
 {
     public class PandaSocialNetwork
     {
